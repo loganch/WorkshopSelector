d222313d51498593a5756bf06bb6b4e987efd5a4
Logan Hampton <losesmc@gmail.com>
2015-10-13 20:48:35 -0700

    Initial commit

---
+++ WorkshopSelector/Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lstWorkshops = New System.Windows.Forms.ListBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lstLocations = New System.Windows.Forms.ListBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.lstCosts = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblTotalCost = New System.Windows.Forms.Label()
+        Me.btnAddWorkshop = New System.Windows.Forms.Button()
+        Me.btnCalculateTotal = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lstWorkshops)
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 24)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(184, 160)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Pick a Workshop"
+        '
+        'lstWorkshops
+        '
+        Me.lstWorkshops.FormattingEnabled = True
+        Me.lstWorkshops.Items.AddRange(New Object() {"Handling Stress", "Time Management", "Supervision Skills", "Negotiation", "How to Interview"})
+        Me.lstWorkshops.Location = New System.Drawing.Point(8, 16)
+        Me.lstWorkshops.Name = "lstWorkshops"
+        Me.lstWorkshops.Size = New System.Drawing.Size(168, 134)
+        Me.lstWorkshops.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lstLocations)
+        Me.GroupBox2.Location = New System.Drawing.Point(224, 24)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(184, 160)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Pick a Location"
+        '
+        'lstLocations
+        '
+        Me.lstLocations.FormattingEnabled = True
+        Me.lstLocations.Items.AddRange(New Object() {"Austin", "Chicago", "Dallas", "Orlando", "Phoenix", "Raleigh"})
+        Me.lstLocations.Location = New System.Drawing.Point(8, 16)
+        Me.lstLocations.Name = "lstLocations"
+        Me.lstLocations.Size = New System.Drawing.Size(168, 134)
+        Me.lstLocations.TabIndex = 1
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.lstCosts)
+        Me.GroupBox3.Location = New System.Drawing.Point(424, 24)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(184, 160)
+        Me.GroupBox3.TabIndex = 0
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "List of Costs"
+        '
+        'lstCosts
+        '
+        Me.lstCosts.FormattingEnabled = True
+        Me.lstCosts.Location = New System.Drawing.Point(8, 16)
+        Me.lstCosts.Name = "lstCosts"
+        Me.lstCosts.Size = New System.Drawing.Size(168, 134)
+        Me.lstCosts.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(224, 200)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Total Cost:"
+        '
+        'lblTotalCost
+        '
+        Me.lblTotalCost.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalCost.Location = New System.Drawing.Point(294, 196)
+        Me.lblTotalCost.Name = "lblTotalCost"
+        Me.lblTotalCost.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotalCost.TabIndex = 4
+        Me.lblTotalCost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnAddWorkshop
+        '
+        Me.btnAddWorkshop.Location = New System.Drawing.Point(44, 232)
+        Me.btnAddWorkshop.Name = "btnAddWorkshop"
+        Me.btnAddWorkshop.Size = New System.Drawing.Size(112, 39)
+        Me.btnAddWorkshop.TabIndex = 5
+        Me.btnAddWorkshop.Text = "&Add Workshop"
+        Me.btnAddWorkshop.UseVisualStyleBackColor = True
+        '
+        'btnCalculateTotal
+        '
+        Me.btnCalculateTotal.Location = New System.Drawing.Point(188, 232)
+        Me.btnCalculateTotal.Name = "btnCalculateTotal"
+        Me.btnCalculateTotal.Size = New System.Drawing.Size(112, 39)
+        Me.btnCalculateTotal.TabIndex = 6
+        Me.btnCalculateTotal.Text = "&Calculate Total"
+        Me.btnCalculateTotal.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(332, 231)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(112, 39)
+        Me.btnReset.TabIndex = 7
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(476, 232)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(112, 39)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(632, 287)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnCalculateTotal)
+        Me.Controls.Add(Me.btnAddWorkshop)
+        Me.Controls.Add(Me.lblTotalCost)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Workshop Selector"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents lstWorkshops As ListBox
+    Friend WithEvents lstLocations As ListBox
+    Friend WithEvents lstCosts As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblTotalCost As Label
+    Friend WithEvents btnAddWorkshop As Button
+    Friend WithEvents btnCalculateTotal As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ WorkshopSelector/Form1.vb
@@ -0,0 +1,80 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Closes the program
+        Me.Close()
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        'Removes items from Cost list, clears the total cost and deselects items in listbox
+        lblTotalCost.Text = String.Empty
+        lstCosts.Items.Clear()
+        lstWorkshops.SelectedIndex = -1
+        lstLocations.SelectedIndex = -1
+    End Sub
+
+    Private Sub btnAddWorkshop_Click(sender As Object, e As EventArgs) Handles btnAddWorkshop.Click
+        Dim intDays As Integer, intRegistrationFee As Integer, intLodgingFee As Integer
+
+        'Setup variables for Workshop
+        Select Case CStr(lstWorkshops.SelectedItem)
+            Case "Handling Stress"
+                intDays = 3
+                intRegistrationFee = 595
+            Case "Time Management"
+                intDays = 3
+                intRegistrationFee = 695
+            Case "Supervision Skills"
+                intDays = 3
+                intRegistrationFee = 995
+            Case "Negotiation"
+                intDays = 5
+                intRegistrationFee = 1295
+            Case "How to Interview"
+                intDays = 1
+                intRegistrationFee = 395
+            Case String.Empty 'If nothing is selected inform user and stop
+                MessageBox.Show("Please select a Workshop")
+                Exit Sub
+            Case Else
+                lblTotalCost.Text = "ERROR!"
+                Exit Sub
+        End Select
+
+        'Setup variables for location
+        Select Case CStr(lstLocations.SelectedItem)
+            Case String.Empty 'If nothing is selected inform user and stop
+                MessageBox.Show("Please select a Location")
+                Exit Sub
+            Case "Austin"
+                intLodgingFee = intDays * 95
+            Case "Chicago"
+                intLodgingFee = intDays * 125
+            Case "Dallas"
+                intLodgingFee = intDays * 110
+            Case "Orlando"
+                intLodgingFee = intDays * 100
+            Case "Phoenix"
+                intLodgingFee = intDays * 92
+            Case "Raleigh"
+                intLodgingFee = intDays * 90
+            Case Else
+                lblTotalCost.Text = "ERROR!"
+                Exit Sub
+        End Select
+
+        'Add the fees and place them in the cost list
+        lstCosts.Items.Add((intLodgingFee + intRegistrationFee).ToString("c"))
+    End Sub
+
+    Private Sub btnCalculateTotal_Click(sender As Object, e As EventArgs) Handles btnCalculateTotal.Click
+        Dim intLoopLimit As Integer = lstCosts.Items.Count - 1, intTotalCosts As Integer = 0
+
+        'Iterate through costs lists and add them to the total
+        For i = 0 To intLoopLimit
+            intTotalCosts += CInt(lstCosts.Items(i))
+        Next
+
+        'Display the total in currency format
+        lblTotalCost.Text = intTotalCosts.ToString("c")
+    End Sub
+End Class
